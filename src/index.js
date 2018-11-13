87a15384f5cdc47a374eac0bb06d044633bc8bb0
kenji <k.saito.1985@gmail.com>
2018-11-14 08:00:58 +0900

    update

--- src/index.js
+++ src/index.js
@@ -3,6 +3,6 @@ console.log('[danshariGL] version: DANSHARI_VERSOIN, %o', 'https://github.com/ke
 export * from './utils/functions/gl-functions';
 export * from './utils/functions/gl-textures';
 export * from './utils/functions/assets-functions';
-export * from './utils/genera-geometry';
+export * from './utils/generate/generate-geometry';
 export * from './camera';
 export * from './math';
