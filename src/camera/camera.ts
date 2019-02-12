dda2561682a2c7666341224b2a00f21cae6d9964
Kenji Saito <k.saito.1985@gmail.com>
2019-02-12 16:14:22 +0900

    update build

--- src/camera/camera.ts
+++ src/camera/camera.ts
@@ -2,28 +2,29 @@ import { mat4 } from 'gl-matrix';
 import { Vector3 } from '../interfaces/interface';
 
 export class Camera {
+	public position: Vector3 = { x: 0, y: 0, z: 0 };
 	private type: string;
-	private position: Vector3 = { x: 0, y: 0, z: 0 };
 	private lookAtPosition: Vector3 = { x: 0, y: 0, z: 0 };
 	protected viewMatrix: mat4 = mat4.create();
 	protected projectionMatrix: mat4 = mat4.create();
+
 	constructor(type: string = 'camera') {
 		this.type = type;
 	}
 
-	updatePosition(xx = 0, yy = 0, zz = 0) {
+	public updatePosition(xx = 0, yy = 0, zz = 0) {
 		this.position.x = xx;
 		this.position.y = yy;
 		this.position.z = zz;
 	}
 
-	updateLookAtPosition(xx = 0, yy = 0, zz = -100) {
+	public updateLookAtPosition(xx = 0, yy = 0, zz = -100) {
 		this.lookAtPosition.x = xx;
 		this.lookAtPosition.y = yy;
 		this.lookAtPosition.z = zz;
 	}
 
-	updateViewMatrix() {
+	public updateViewMatrix() {
 		mat4.lookAt(
 			this.viewMatrix,
 			[this.position.x, this.position.y, this.position.z],
@@ -45,7 +46,13 @@ export class PerspectiveCamera extends Camera {
 		this.updatePerspective(width, height, fov, near, far);
 	}
 
-	updatePerspective(width: number, height: number, fov: number, near: number, far: number) {
+	public updatePerspective(
+		width: number,
+		height: number,
+		fov: number,
+		near: number,
+		far: number
+	) {
 		mat4.perspective(this.projectionMatrix, (fov / 180) * Math.PI, width / height, near, far);
 	}
 }
@@ -63,7 +70,7 @@ export class OrthoCamera extends Camera {
 		this.updatePerspective(left, right, bottom, top, near, far);
 	}
 
-	updatePerspective(
+	public updatePerspective(
 		left: number,
 		right: number,
 		bottom: number,
